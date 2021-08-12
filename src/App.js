9130c883eaf0ea9131baf1e322effd5735351ebb
unknown <sumax333@gmail.com>
2021-08-12 16:57:11 +0530

    completed v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import Message from "./components/Message";
+
+const CHARS_PER_STROKE = 5;
 
 function App() {
+  const [sourceCode, setSourceCode] = useState("");
+  const [content, setContent] = useState("");
+  const [messageType, setMessageType] = useState("denied");
+
+  const scrollToRef = useRef();
+
+  // let startIndex = 0;
+  // let currentIndex = 0;
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const [isLocked, setIsLocked] = useState(false);
+  useEffect(() => {
+    fetch("code.txt")
+      .then((r) => r.text())
+      .then((text) => {
+        setSourceCode(text);
+      });
+  }, []);
+
+  const handleKeyDown = (event) => {
+    if (event.key !== "Escape") runScript();
+    else removeMessage();
+  };
+
+  const removeMessage = (params) => {
+    setIsLocked(false);
+  };
+
+  const runScript = () => {
+    if (!isLocked) {
+      // get more text and update the cursor
+      setCurrentIndex(currentIndex + CHARS_PER_STROKE);
+      setContent(sourceCode.substring(0, currentIndex));
+
+      scrollToRef.current.scrollIntoView();
+      // update access message
+      if (currentIndex !== 0 && currentIndex % 300 === 0) {
+        setIsLocked(true);
+        setMessageType("denied");
+      }
+      if (currentIndex !== 0 && currentIndex % 900 === 0) {
+        setIsLocked(true);
+        setMessageType("success");
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div
+        id="container"
+        onKeyDown={handleKeyDown}
+        tabIndex={0}
+        className={isLocked ? "blurred" : ""}
+      >
+        <div id="source">{content}</div>
+        <p ref={scrollToRef}></p>
+      </div>
+      {isLocked && <Message type={messageType} />}
+    </>
   );
 }
 
